2ff48e918c33731995875d34be00586a8e1f4cea
Cody Silby <codysilby@mmc.school.nz>
2019-06-12 10:45:02 +1200

    Add files via upload

---
+++ 2018_Level2_Dodge/2018_Level2_Dodge/Planet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace _2018_Level2_Dodge
+{
+    class Planet
+    {
+        public int x, y, width, height;
+        public Image planetImage;
+
+        public Rectangle planetRec;
+        public int score;
+
+        public Planet(int spacing)
+        {
+            x = spacing;
+            y = 10;
+            width = 20;
+            height = 20;
+            planetImage = Image.FromFile("planet1.png");
+            planetRec = new Rectangle(x, y, width, height);
+        }
+        public void drawPlanet(Graphics g)
+        {
+            planetRec = new Rectangle(x, y, width, height);
+            g.DrawImage(planetImage, planetRec);
+        }
+        public void movePlanet()
+        {
+            y += 5;
+
+            planetRec.Location = new Point(x, y);
+
+            if (planetRec.Location.Y > 400)
+            {
+                y = 20;
+                planetRec.Location = new Point(x, y);
+            }
+
+        }
+
+
+    }
+}
---
+++ 2018_Level2_Dodge/2018_Level2_Dodge/frmDodge.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2018_Level2_Dodge
+{
+    public partial class frmDodge : Form
+    {
+        Graphics g;
+        Planet[] planet = new Planet[7];
+        public frmDodge()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 7; i++)
+            {
+                int x = 10 + (i * 75);
+                planet[i] = new Planet(x);
+            }
+
+
+        }
+
+        private void frmDodge_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlGame_Paint(object sender, PaintEventArgs e)
+        {
+            //get the graphics used to paint on the panel control
+            g = e.Graphics;
+            //call the Planet class's DrawPlanet method to draw the image planet1 
+            for (int i = 0; i < 7; i++)
+            {
+                //call the Planet class's drawPlanet method to draw the images
+                planet[i].drawPlanet(g);
+            }
+
+
+        }
+
+        private void tmrPlanet_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                planet[i].movePlanet();
+            }
+            pnlGame.Invalidate();//makes the paint event fire to redraw the panel
+        }
+    }
+}
